9dbe34a7a4db59c926779aa73b4531b5f6a5a0d6
Kallin Khan <kallinkhan@gmail.com>
2017-09-25 17:29:58 -0500

    setup

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../../../Documents/data-exploration/edammo_dashboard/app/components/App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
