42db3d5123a346f2bceb87616f4aa77771dee110
Kallin Khan <kallinkhan@gmail.com>
2017-09-25 17:44:21 -0500

    created build

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from '../../../Documents/data-exploration/edammo_dashboard/app/components/App';
+import App from './components/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
